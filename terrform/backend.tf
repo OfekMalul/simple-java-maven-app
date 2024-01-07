5f2dba6415b92fb0e57705e097ac169ed1a4bbdc
Ofek Malul <ofekmalulmail@gmail.com>
2024-01-07 16:02:57 +0200

    fix

--- terrform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "ofek-malul-terraform-bucket"
-    key    = "terraform/terraform-java-hellowolrd-project"
-    region = "us-east-1"
-  }
-}
--- terrform/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "instance_name" {
-  description = "Name of my ec2 instance"
-  type        = string
-  default     = "Java Hello World EC2"
-}
